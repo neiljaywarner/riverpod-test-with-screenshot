ac8942192af5e0cfbb724c7c09e613c58748071e
Neil.Warner <neil.warner@everbridge.com>
2023-10-13 19:37:38 -0500

    small modification for updated script to save in project root on local host machine.

---
+++ integration_test/readme.md
@@ -0,0 +1,3 @@
+* screenshots with flutter drive \
+  --driver=test_driver/integration_test.dart \
+  --target=integration_test/favorite_button_test.dart
--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,20 +1,14 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
-  try {
-    await integrationDriver(
-      // (Optional): triggers on every screenshot
-      onScreenshot: (String name, List<int> bytes) async {
-        final path = 'screenshots/$name.png';
-        final image = await File(path).create(recursive: true);
-        image.writeAsBytesSync(bytes);
-        return true;
-      },
-    );
-  } catch (e) {
-    if (kDebugMode) print('Error occured: $e');
-  }
+  await integrationDriver(
+    onScreenshot: (String screenshotName, List<int> screenshotBytes, [Map<String, Object?>? args]) async {
+      final File image = File('${Platform.operatingSystem}_$screenshotName.png');
+      image.writeAsBytesSync(screenshotBytes);
+      // Return false if the screenshot is invalid.
+      return true;
+    },
+  );
 }
